3b8a9a1b8bbceebb45543d25ba096fc2d182117e
zell.christopher <zelldon91@gmail.com>
2015-03-12 10:55:07 +0100

    added documentation

--- Flashlight/src/de/zell/camera/CameraApiFactory.java
+++ Flashlight/src/de/zell/camera/CameraApiFactory.java
@@ -21,11 +21,18 @@ import de.zell.camera.api.CameraOldApi;
 import de.zell.camera.api2.CameraNewApi;
 
 /**
- *
+ * Represents the CamaeraApiFactory which gives for the corresponding
+ * build version the correct camera api object.
+ * 
  * @author Christopher Zell <zelldon91@googlemail.com>
  */
 public class CameraApiFactory {
 
+  /**
+   * The correct camera api for the corresponding build version.
+   * 
+   * @return the camera api object
+   */
   public static CameraApi produceCameraApi() {
     if (Build.VERSION.SDK_INT < 21) {
       return new CameraOldApi();
